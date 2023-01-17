ad59a51a01fd32a615a7041924c10861843648dc
mohammad <mdfa1381@gmail.com>
2023-01-17 02:43:55 -0800

    login form

--- backend/server.js
+++ backend/server.js
@@ -5,29 +5,21 @@ import "dotenv/config";
 import { connectDB } from "./config/dbConfig.js";
 import mongoose from "mongoose";
 import userRoutes from "./routes/api/userRoutes.js";
-import cookieParser from "cookie-parser"
-import cors from 'cors'
-import corsOptions from './config/corsOptions.js';
-import credentials from './middlewares/credentials.js';
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import corsOptions from "./config/corsOptions.js";
+import credentials from "./middlewares/credentials.js";
 
 const app = express();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 connectDB();
-app.use(credentials)
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(cookieParser())
-app.use('/user', userRoutes)
-
-
-
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/user", userRoutes);
 
 mongoose.connection.once("open", () => {
-    console.log('connected to mongoDB')
-    app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
-
-})
-
-// client id = 
-// client server =  
+  console.log("connected to mongoDB");
+  app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
+});
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+        <App/>
+          </BrowserRouter>
 );
